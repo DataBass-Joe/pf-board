4acd186977f97cadded3e5a6967c25900d87dcaa
Joseph Luker <josephsluker@gmail.com>
2021-02-10 09:34:38 -0700

    And now it's deploying! Lets get this on github

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
+    <div id="nav"> <!--v-bind:style="styleObject">-->
+      <router-link to="/home">Home</router-link>
       |
       <router-link to="/about">About</router-link>
       |
       <router-link to="/bestiary">Bestiary</router-link>
+      |
+      <router-link to="/">Hide</router-link>
     </div>
     <router-view/>
   </div>
@@ -14,21 +16,84 @@
 
 <script>
 
+//
+//function getAverageRGB(imgEl) {
+//
+//  var blockSize = 5, // only visit every 5 pixels
+//      defaultRGB = {r: 0, g: 0, b: 0}, // for non-supporting envs
+//      canvas = document.createElement('canvas'),
+//      context = canvas.getContext && canvas.getContext('2d'),
+//      data, width, height,
+//      i = -4,
+//      length,
+//      rgb = {r: 0, g: 0, b: 0},
+//      count = 0;
+//
+//  if (!context) {
+//    return defaultRGB;
+//  }
+//
+//  height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+//  width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+//
+//  context.drawImage(imgEl, 0, 0);
+//
+//  try {
+//    data = context.getImageData(0, 0, width, height);
+//  } catch (e) {
+//    /* security error, img on diff domain */
+//    return defaultRGB;
+//  }
+//
+//  length = data.data.length;
+//
+//  while ((i += blockSize * 4) < length) {
+//    ++count;
+//    rgb.r += data.data[i];
+//    rgb.g += data.data[i + 1];
+//    rgb.b += data.data[i + 2];
+//  }
+//
+//  // ~~ used to floor values
+//  rgb.r = ~~(rgb.r / count);
+//  rgb.g = ~~(rgb.g / count);
+//  rgb.b = ~~(rgb.b / count);
+//
+//  return rgb;
+//
+//}
+//
+//let r = getAverageRGB("assets/j-jp-.jpg")['r']
+//let g = getAverageRGB("assets/j-jp-.jpg")['g']
+//let b = getAverageRGB("assets/j-jp-.jpg")['b']
+//
+//let x = ''
+
 
 export default {
   name: 'App'
+  // ,
+  // data: function () {
+  //   return {
+  //     styleObject: {"background-color": x.concat("rgba(", r, ',', g, ',', b, ',.5')}
+  //
+  //   };
+  // }
 }
 
 </script>
 
+
 <style>
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 
 html {
@@ -47,7 +112,7 @@ html {
   box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
   color: #600;
   display: flex;
-  background-color: rgba(255, 127, 127, 0.5);
+  /*background-color: rgba(255, 127, 127, 0.75);*/
   margin: 1vmin;
   align-items: center;
 }
@@ -60,14 +125,19 @@ body {
 
 #nav {
   padding: 30px;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+  background-color: rgba(255, 127, 127, 0.90);
+
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #ffffff;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #000000;
 }
 </style>
--- src/components/Bestiary.vue
+++ src/components/Bestiary.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <span v-html="pg[0]['fulltext']"></span>
+      <span id="stat-block" v-html="pg[0]['fulltext']"></span>
   </div>
 </template>
 
@@ -17,7 +17,6 @@ export default {
         query: {select: ['id', 'name', 'fulltext']},
         limit: 1
       }
-
     }
   }, computed: {
     bestiary() {
@@ -31,7 +30,86 @@ export default {
 
 </script>
 
-<style scoped>
+<style>
 
 
+
+
+
+#stat-block {
+  padding: 1vmin;
+  text-align: left;
+  max-width: 50vmax;
+  min-width: 50px;
+  text-shadow: 2px 2px 4px #000000;
+  color:white;
+}
+h5 {
+  font-size: 16px;
+  font-weight: normal;
+  padding:0;
+  margin:1vmin;
+}
+
+
+div.heading {
+  height: 30px;
+}
+
+h1 {
+  font-size: 22px;
+}
+
+h2 {
+  font-size: 22px;
+  text-align: left;
+}
+
+h3 {
+  font-size: 16px;
+  font-weight: normal;
+}
+
+h4 {
+  text-align: justify;
+  font-size: 16px;
+  font-weight: normal;
+}
+
+p {
+  margin: 0 0 1px;
+  padding: 0 0 1px;
+  color: white;
+}
+
+h6 {
+
+  margin-left: 30px;
+  font-size: 16px;
+  font-weight: normal;
+}
+
+p.alignleft {
+  margin-left: 0;
+  text-indent: 10px;
+  float: left;
+  text-align: left;
+  font-size: 18px;
+  font-weight: bold;
+  padding: 3px;
+}
+
+p.alignright {
+  float: right;
+  text-align: right;
+  font-size: 18px;
+  font-weight: bold;
+  text-indent: 10px;
+
+  padding: 3px;
+}
+
+li {
+  text-indent: 0;
+}
 </style>
